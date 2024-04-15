1c1a6e649a895c6b9a57f3a7691bac88b79a0329
Noah Tah <noahtah21@gmail.com>
2024-04-14 21:32:56 -0500

    done for the night, made a rough toolbar

--- src/whitenoise/WhiteNoiseAppGUI.java
+++ src/whitenoise/WhiteNoiseAppGUI.java
@@ -18,25 +18,26 @@ public class WhiteNoiseAppGUI {
         button.setPreferredSize(new Dimension(20, 20)); // the change added
         WhiteNoiseApp noiseApp = new WhiteNoiseApp(); // Instantiate the White noise app
 
-        button.addActionListener(e -> {
-            if (button.getText().equals("Start")) {
-                noiseApp.startNoise(); // Start the noise
-                button.setText("Stop");
-            } else {
-                noiseApp.stopNoise(); // Stop the noise
-                button.setText("Start");
-            }
-        });
-        frame.add(button, BorderLayout.NORTH); // add button to bottom of the frame
-        frame.getContentPane().add(button);
-        frame.pack();
-        frame.setVisible(true);
+        // button.addActionListener(e -> {
+        //     if (button.getText().equals("Start")) {
+        //         noiseApp.startNoise(); // Start the noise
+        //         button.setText("Stop");
+        //     } else {
+        //         noiseApp.stopNoise(); // Stop the noise
+        //         button.setText("Start");
+        //     }
+        // });
+        // frame.add(button, BorderLayout.NORTH); // add button to bottom of the frame
+        // frame.getContentPane().add(button);
+        // frame.pack();
+        // frame.setVisible(true);
         // frame.getContentPane().setBackground(Color.pink); // Eventually change the background color.
 
         VolumeSlider volumeSlider = new VolumeSlider(noiseApp);
-
+        ToolBar toolbar = new ToolBar(noiseApp);
+        frame.add(toolbar, BorderLayout.SOUTH);
         // Add the volume slider to center of the frame
-        frame.add(volumeSlider, BorderLayout.SOUTH);
+        // frame.add(volumeSlider, BorderLayout.SOUTH);
         frame.setSize(420, 420); // Adjusts the window to fit the preferred size and layouts of it's subcomponents
         frame.setVisible(true);
     }
