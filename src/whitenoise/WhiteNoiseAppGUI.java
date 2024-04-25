18db644459e6bfd4a66834abb4dc3d257bf1c8fa
Noah Tah <noahtah21@gmail.com>
2024-04-25 10:26:31 -0500

    trying to push??

--- src/whitenoise/WhiteNoiseAppGUI.java
+++ src/whitenoise/WhiteNoiseAppGUI.java
@@ -1,47 +1,55 @@
 package whitenoise;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import javax.swing.JPanel;
 import javax.swing.JFrame;
-import javax.swing.JButton;
 import javax.swing.SwingUtilities;
 import java.awt.*;
 
 public class WhiteNoiseAppGUI {
     
     private static void createAndShowGUI() {
-        // Create the main window (frame)
+        WhiteNoiseApp noiseApp = new WhiteNoiseApp(); // Instantiate the White noise app        
+        
+        
+        // JFrame & BorderLayout()
         JFrame frame =  new JFrame("White Noise Generator");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.setLayout(new BorderLayout()); // use BorderLayout to organize button and slider
+        frame.setSize(1200, 700);  // default size of application
+        frame.setLayout(new BorderLayout(10,10)); // CAN ADD A GAP BETWEEN PANELS HERE LATER IF I WANT
+        //-----------------------------------------------------------------//
+        // Toolbar & Panels 
+        ToolBar toolbarPanel = new ToolBar(noiseApp); // instantiate toolbar
+        JPanel leftPanel = new JPanel();
+        JPanel rightPanel = new JPanel();
+        JPanel topPanel = new JPanel();
+        JPanel centerPanel = new JPanel();
+
+        leftPanel.setPreferredSize(new Dimension(50,100));
+        rightPanel.setPreferredSize(new Dimension(50,100));
+        topPanel.setPreferredSize(new Dimension(100,100));
+        centerPanel.setPreferredSize(new Dimension(100,100));
+        
+        toolbarPanel.setBackground(Color.WHITE);
+        leftPanel.setBackground(Color.LIGHT_GRAY);
+        rightPanel.setBackground(Color.LIGHT_GRAY);
+        topPanel.setBackground(Color.LIGHT_GRAY);
+        centerPanel.setBackground(Color.LIGHT_GRAY);
 
 
-        JButton button = new JButton("Start"); // button to start/stop white noise
-        button.setPreferredSize(new Dimension(20, 20)); // the change added
-        WhiteNoiseApp noiseApp = new WhiteNoiseApp(); // Instantiate the White noise app
+        frame.add(toolbarPanel, BorderLayout.SOUTH); // ADD TOOLBAR BUT MAYBE ME WE DO A PANEL HERE INSTEAD POTENTIALLY
+        frame.add(leftPanel, BorderLayout.WEST);
+        frame.add(rightPanel, BorderLayout.EAST);
+        frame.add(topPanel, BorderLayout.NORTH);
+        frame.add(centerPanel, BorderLayout.CENTER);
+        
 
-        // button.addActionListener(e -> {
-        //     if (button.getText().equals("Start")) {
-        //         noiseApp.startNoise(); // Start the noise
-        //         button.setText("Stop");
-        //     } else {
-        //         noiseApp.stopNoise(); // Stop the noise
-        //         button.setText("Start");
-        //     }
-        // });
-        // frame.add(button, BorderLayout.NORTH); // add button to bottom of the frame
-        // frame.getContentPane().add(button);
-        // frame.pack();
-        // frame.setVisible(true);
-        // frame.getContentPane().setBackground(Color.pink); // Eventually change the background color.
-
-        VolumeSlider volumeSlider = new VolumeSlider(noiseApp);
-        ToolBar toolbar = new ToolBar(noiseApp);
-        frame.add(toolbar, BorderLayout.SOUTH);
-        // Add the volume slider to center of the frame
-        // frame.add(volumeSlider, BorderLayout.SOUTH);
-        frame.setSize(420, 420); // Adjusts the window to fit the preferred size and layouts of it's subcomponents
+        
         frame.setVisible(true);
     }
 
+    // ------------------------------------------------------------//
     public static void main(String[] args) {
         // Esnrue the GUI Construction is done on the Event Dispatch Thread
         SwingUtilities.invokeLater(() -> createAndShowGUI());
